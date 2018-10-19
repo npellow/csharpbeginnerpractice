50fd773b1da957965a34707d1e7d1768593ed413
Nichole Pellow <ladypellow@gmail.com>
2018-10-19 10:33:47 -0400

    Section 4
    Lecture 33: Demo: Strings practice file

---
+++ SectionFourStrings/SectionFourStrings/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SectionFourStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstName = "Nichole";
+            var lastName = "Pellow";
+
+            //using concat to combine strings
+            var myName = firstName + " " + lastName;
+            //using .format to combine strings
+            var myFullName = string.Format("My name is {0} {1}.", firstName, lastName);
+
+            //array of names
+            var names = new string[3] { "Elsie", "John Avery", "Mae" };
+            var formattedNames = string.Join(", ", names);
+            //Console.WriteLine(formattedNames);
+
+            //verbatim strings
+            //var text = "Hi John\nLook into the following paths\nc:\\folder1\\folder2\nc:\\folder3\\folder4";
+            //clean it up using verbatim
+            var text = @"Hi John
+Look into the following paths
+c:\folder1\folder2
+c:\folder3\folder4";
+            Console.WriteLine(text);
+        }
+    }
+}
