4bd367879bb5378b2d02ec7f910c439058b78f20
Nichole Pellow <ladypellow@gmail.com>
2018-10-19 08:41:42 -0400

    Sections 1 - 3
    practice files from the first 3 sections of the course

---
+++ TypeConversion/TypeConversion/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //implicit type conversion
+            /*byte b = 1;
+            int i = b;
+            Console.WriteLine(i);*/
+
+            //explicit type conversion
+            /*int i = 1;
+            byte b = (byte)i;
+            Console.WriteLine(b);*/
+
+            //non-compatible type conversion
+            /*string number = "1234";
+            int i = Convert.ToInt32(number);
+            Console.WriteLine(i);
+
+            //using an exception with non-compatible type conversion
+            try
+            {
+                string numbers = "1234";
+                byte b = Convert.ToByte(numbers);
+                Console.WriteLine(b);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The number could not be converted to a byte.");
+            }*/
+
+            //using an exception with a string to bool conversion
+            try
+            {
+                string str = "true";
+                bool b = Convert.ToBoolean(str);
+                Console.WriteLine(b);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The string could not be converted.");
+            }
+            
+
+        }
+    }
+}
---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 
+             * Mosh erased the following;
+             * keeping as examples.
+             * 
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'A';
+            string firstName = "nichole";
+            bool isWorking = false;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking); */
+
+            /*
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue); */
+
+            const float Pi = 3.14f;
+        } 
+    }
+}
