2c152e23a2bbbed704ba54778f324488d04e123c
Nichole Pellow <ladypellow@gmail.com>
2018-10-21 13:18:28 -0400

    Section 5: Conditional Exercises
    Exercise 2

---
+++ Lecture43Exercise2/Lecture43Exercise2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture43Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            var a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter another number: ");
+            var b = Convert.ToInt32(Console.ReadLine());
+
+            var max = (a > b) ? a : b;
+            Console.WriteLine("The max number is: " + max);
+        }
+    }
+}
