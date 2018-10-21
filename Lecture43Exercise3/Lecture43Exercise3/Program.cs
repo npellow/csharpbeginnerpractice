142ef3cab2f6f93627b5e43572361117f1d8c842
Nichole Pellow <ladypellow@gmail.com>
2018-10-21 13:24:48 -0400

    Section 5: Conditional Exercises
    Exercise 3

---
+++ Lecture43Exercise3/Lecture43Exercise3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture43Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Find a picture... What is the length: ");
+            var length = Convert.ToInt64(Console.ReadLine());
+
+            Console.Write("Now enter the width: ");
+            var width = Convert.ToInt64(Console.ReadLine());
+
+            if (length > width)
+                Console.WriteLine("This is a portrait");
+            else
+                Console.WriteLine("This is a landscape");
+        }
+    }
+}
