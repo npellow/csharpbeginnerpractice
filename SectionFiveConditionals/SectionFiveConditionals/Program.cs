2827466067ac5cdef5b717df333531a3a134d5af
Nichole Pellow <ladypellow@gmail.com>
2018-10-21 12:41:50 -0400

    Section 5
    Lecture 42: Demo: If/Else and Switch/Case practice files

---
+++ SectionFiveConditionals/SectionFiveConditionals/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SectionFiveConditionals
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            /*
+            int hour = 10;
+
+            if (hour > 0 && hour < 12)
+            {
+                Console.WriteLine("It's morning.");
+            }
+            else if (hour >= 12 && hour < 18)
+            {
+                Console.WriteLine("It's afternoon.");
+            }
+            else
+            {
+                Console.WriteLine("It's evening.");
+            }
+            */
+
+            //bool isGoldCustomer = true;
+
+            /*float price;
+            if (isGoldCustomer)
+                price = 19.95f;
+            else
+                price = 29.95f;*/
+
+            /*float price = (isGoldCustomer) ? 19.95f : 29.95f;
+
+            Console.WriteLine(price);*/
+
+            var season = Season.Autumn;
+
+            switch (season)
+            {
+                case Season.Autumn:
+                case Season.Winter:
+                    Console.WriteLine("We've got sales!");
+                    break;
+
+                case Season.Spring:
+                    Console.WriteLine("It's spring");
+                    break;
+
+                default:
+                    Console.WriteLine("It's some sorta time of year");
+                    break;
+
+            }
+
+        }
+    }
+}
---
+++ SectionFiveConditionals/SectionFiveConditionals/Season.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SectionFiveConditionals
+{
+    public enum Season
+    {
+        Spring,
+        Summer,
+        Autumn,
+        Winter
+    }
+}
