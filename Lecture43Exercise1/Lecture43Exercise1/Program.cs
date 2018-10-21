4f44ccd09faf9ef73d2b9751e2c0c4e7f2a0e19b
Nichole Pellow <ladypellow@gmail.com>
2018-10-21 13:10:10 -0400

    Section 5: Conditional Exercises
    Exercise 1 of Conditional Exercises

---
+++ Lecture43Exercise1/Lecture43Exercise1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture43Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number between 1 and 10: ");
+
+            var inputNum = Console.ReadLine();
+            var number = Convert.ToInt32(inputNum);
+
+            if (number >= 1 && number <= 10)
+                Console.WriteLine("Valid");
+            else
+                Console.WriteLine("Invalid");
+        }
+    }
+}
