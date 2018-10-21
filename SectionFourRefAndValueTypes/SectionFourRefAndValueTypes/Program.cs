c9d35a83611d4013823f7c63b6f656e5c337ed86
Nichole Pellow <ladypellow@gmail.com>
2018-10-20 22:54:18 -0400

    Section 4
    Lecture 37: Demo: Reference Types and Value Types practice file

---
+++ SectionFourRefAndValueTypes/SectionFourRefAndValueTypes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SectionFourRefAndValueTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //simple example of value types in action
+            var a = 10;
+            var b = a;
+            b++;
+            Console.WriteLine(string.Format("a: {0} b: {1}", a, b));
+
+            //simple example of reference types in action
+            var array1 = new int[3] {1, 2 , 3};
+            var array2 = array1;
+            array2[0] = 0;
+            Console.WriteLine(string.Format("array1[0]: {0} array2[0]: {1}", array1[0], array2[0]));
+        }
+    }
+}
