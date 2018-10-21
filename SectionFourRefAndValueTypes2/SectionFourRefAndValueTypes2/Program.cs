ae2db2bf6e191cd9e61f93cf56c8f4ac56f3e053
Nichole Pellow <ladypellow@gmail.com>
2018-10-20 23:05:45 -0400

    Section 4
    Lecture 38: Demo: Reference Types and Value Types practice file

---
+++ SectionFourRefAndValueTypes2/SectionFourRefAndValueTypes2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SectionFourRefAndValueTypes2
+{
+    public class Person
+    {
+        public int Age;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //value type example
+            var number = 1;
+            Increment(number);
+            Console.WriteLine(number);
+
+            //reference type example
+            var person = new Person() { Age = 20};
+            MakeOld(person);
+            Console.WriteLine(person.Age);
+        }
+
+        public static void Increment(int number)
+        {
+            number += 10;
+        }
+
+        public static void MakeOld(Person person)
+        {
+            person.Age += 10;
+        }
+    }
+}
