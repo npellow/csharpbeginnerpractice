9c0bd6f57d3b461c057c92f408ca548f4b1ec025
Nichole Pellow <ladypellow@gmail.com>
2018-10-22 08:41:43 -0400

    Section 5
    Lecture 45: Demo: For Loops practice file

---
+++ Section5ForLoops/Section5ForLoops/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section5ForLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 1; i <= 10; i++)
+            {
+                if (i%2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            for (var j = 10; j >= 1; j--)
+            {
+                if (j%2 == 0)
+                {
+                    Console.WriteLine(j);
+                }
+            }
+        }
+    }
+}
