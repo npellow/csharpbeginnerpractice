cc70942ccf715b9dadff15e158d61c60ce07e1ed
Nichole Pellow <ladypellow@gmail.com>
2018-10-22 08:29:15 -0400

    Section 5: Conditional Exercises
    Exercise 4

---
+++ Lecture43Exercise4/Lecture43Exercise4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture43Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is the speed limit: ");
+            var speedLimit = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("How fast is the car going: ");
+            var carSpeed = Convert.ToInt32(Console.ReadLine());
+
+            if (carSpeed < speedLimit)
+            {
+                Console.WriteLine("Good job staying under the speed limit.");
+            }
+            else
+            {
+                const int kmPerDemeritPoint = 5;
+                var demeritPoints = (carSpeed - speedLimit) / kmPerDemeritPoint;
+
+                if (demeritPoints > 12)
+                {
+                    Console.WriteLine("Oh no, you're going too fast! License now suspended!");
+                }
+                else
+                {
+                    Console.WriteLine("Slow it down. Demerit points: " + demeritPoints);
+                }
+            }
+        }
+    }
+}
