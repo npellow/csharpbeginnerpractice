097c43806019323f1e3ecbfaaff6b8b29359adf4
Nichole Pellow <ladypellow@gmail.com>
2018-10-22 08:51:18 -0400

    Section 5
    Lecture 46: Demo: Foreach Loops practice file

---
+++ Section5ForeachLoops/Section5ForeachLoops/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section5ForeachLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* var name = "John Smith";
+
+            //for loop example to be simplified after with a foreach loop
+            for (var i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine(name[i]);
+            }
+
+            Console.WriteLine("\b");
+
+            //simplified foreach loop
+            foreach (var character in name)
+            {
+                Console.WriteLine(character);
+            } */
+
+            //foreach with an array
+            var numbers = new int[] { 1, 2, 3, 4 };
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
