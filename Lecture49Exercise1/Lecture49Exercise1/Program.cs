ddb9f00af71093bb1f1aa45c3f1b7f4c5f41137a
Nichole Pellow <ladypellow@gmail.com>
2018-10-26 12:34:49 -0400

    Lecture 49: Loop Exercises
    Exercise 1

---
+++ Lecture49Exercise1/Lecture49Exercise1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture49Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = 0;
+
+            for (var i = 0; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                    count++;
+            }
+
+            Console.WriteLine("There are {0} numbers divisble by 3 between 1 and 100.", count);
+        }
+    }
+}
