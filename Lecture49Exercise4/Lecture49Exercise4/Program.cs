a806ff981a70956b6138837b392bfca6ef1e6ba8
Nichole Pellow <ladypellow@gmail.com>
2018-10-26 13:50:39 -0400

    Lecture 49: Loop Exercises
    Exercise 4

---
+++ Lecture49Exercise4/Lecture49Exercise4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture49Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = new Random().Next(1, 10);
+
+            for (var i = 0; i < 4; i++)
+            {
+                Console.Write("Guess the secret number: ");
+                var guess = Convert.ToInt32(Console.ReadLine());
+
+                if (guess == number)
+                {
+                    Console.WriteLine("You won!");
+                    return;
+                }
+            }
+
+            Console.WriteLine("You lost! \nSecret number is " + number + ".");
+        }
+    }
+}
