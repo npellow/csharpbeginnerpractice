83558b1e25645fd48da53978443404fa814d3249
Nichole Pellow <ladypellow@gmail.com>
2018-10-26 13:32:56 -0400

    Lecture 49: Loop Exercises
    Exercise 3

---
+++ Lecture49Exercise3/Lecture49Exercise3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture49Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            var number = Convert.ToInt32(Console.ReadLine());
+
+            var factorial = 1;
+            for (var i = 1; i <= number; i++)
+                factorial *= 1;
+
+            Console.WriteLine("{0}! = {1}", number, factorial);
+        }
+    }
+}
