7f24ddfdbe376e6dce7db831660e5396564128d2
Nichole Pellow <ladypellow@gmail.com>
2018-10-26 13:02:05 -0400

    Lecture 49: Loop Exercises
    Exercise 2

---
+++ Lecture49Exercise2/Lecture49Exercise2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture49Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sum = 0;
+            while (true)
+            {
+                Console.Write("Enter a number, or 'ok' to exit: ");
+                var input = Console.ReadLine();
+
+                if (input.ToLower() == "ok")
+                    break;
+
+                sum += Convert.ToInt32(input);
+            }
+
+            Console.WriteLine("The sum of all numbers is: " + sum);
+        }
+    }
+}
