175476a9efaf4207f16c786770f5bf0ea76920a7
Nichole Pellow <ladypellow@gmail.com>
2018-10-26 14:12:46 -0400

    Lecture 49: Loop Exercises
    Exercise 5

---
+++ Lecture49Exercise5/Lecture49Exercise5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture49Exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter numbers separated by commas: ");
+            var input = Console.ReadLine();
+
+            var numbers = input.Split(',');
+
+            var max = Convert.ToInt32(numbers[0]);
+
+            foreach (var str in numbers)
+            {
+                var number = Convert.ToInt32(str);
+                if (number > max)
+                    max = number;
+            }
+
+            Console.WriteLine("The max number is " + max);
+        }
+    }
+}
