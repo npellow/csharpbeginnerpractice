b31aa11bf615862749569fefa4d75dd240871df5
Nichole Pellow <ladypellow@gmail.com>
2018-10-26 23:13:35 -0400

    Section 6
    Lecture 55: Demo: Lists practice files

---
+++ Section6Lists/Section6Lists/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section6Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>() { 1, 2, 3, 4 };
+            numbers.Add(1);
+            numbers.AddRange(new int[3] { 5, 6, 7 });
+
+            foreach (var n in numbers)
+                Console.WriteLine(n);
+
+
+            Console.WriteLine();
+            // IndexOf()
+            Console.WriteLine("Index of 1: " + numbers.IndexOf(1));
+            Console.WriteLine("Last Index of 1: " + numbers.LastIndexOf(1));
+
+            // Count
+            Console.WriteLine("Count: " + numbers.Count);
+
+            // Remove()
+            for (var i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == 1)
+                    numbers.Remove(numbers[i]);
+            }
+            foreach (var n in numbers)
+                Console.WriteLine(n);
+
+            // Clear()
+            numbers.Clear();
+            Console.WriteLine("Count: " + numbers.Count);
+
+
+
+        }
+    }
+}
