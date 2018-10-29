32e820f51a430b6760eec3a4bb16d15df09b9ecd
Nichole Pellow <ladypellow@gmail.com>
2018-10-29 09:20:12 -0400

    Section 6 Arrays and Lists Exercises
    Exercise 4

---
+++ Lecture56Exercise4/Lecture56Exercise4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture56Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>();
+
+            while (true)
+            {
+                Console.Write("Enter a number (or type 'quit' to exit): ");
+                var input = Console.ReadLine();
+
+                if (input.ToLower() == "quit")
+                    break;
+
+                numbers.Add(Convert.ToInt32(input));
+            }
+
+            var uniques = new List<int>();
+            foreach (var number in numbers)
+            {
+                if (!uniques.Contains(number))
+                    uniques.Add(number);
+            }
+
+            Console.WriteLine("Unique numbers: ");
+            foreach (var number in uniques)
+                Console.WriteLine(number);
+        }
+    }
+}
