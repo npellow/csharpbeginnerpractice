e9b3921d6bd4e08b5b0ba46abdd98be8ba4ba742
Nichole Pellow <ladypellow@gmail.com>
2018-10-29 09:33:20 -0400

    Section 6 Arrays and Lists Exercises
    Exercise 5

---
+++ Lecture56Exercise5/Lecture56Exercise5/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture56Exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] elements;
+            while (true)
+            {
+                Console.Write("Enter a list of numbers separated by a comma: ");
+                var input = Console.ReadLine();
+
+                if (!String.IsNullOrWhiteSpace(input))
+                {
+                    elements = input.Split(',');
+                    if (elements.Length >= 5)
+                        break;
+                }
+
+                Console.WriteLine("Invalid list");
+            }
+
+            var numbers = new List<int>();
+            foreach (var number in elements)
+                numbers.Add(Convert.ToInt32(number));
+
+            var smallests = new List<int>();
+            while (smallests.Count < 3)
+            {
+                var min = numbers[0];
+                foreach (var number in numbers)
+                {
+                    if (number < min)
+                        min = number;
+                }
+
+                smallests.Add(min);
+
+                numbers.Remove(min);
+            }
+
+            Console.WriteLine("The 3 smallest numbers are: ");
+            foreach (var number in smallests)
+                Console.WriteLine(number);
+
+        }
+    }
+}
