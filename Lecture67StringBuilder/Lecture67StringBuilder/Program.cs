c64695db1831df2393d6af191b579038c3e69eca
Nichole Pellow <ladypellow@gmail.com>
2018-11-02 10:33:30 -0400

    Section 8
    Lecture 67: StringBuilder demo: practice files

---
+++ Lecture67StringBuilder/Lecture67StringBuilder/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture67StringBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new StringBuilder();
+            builder
+                .Append('-', 10)
+                .AppendLine()
+                .Append("Header")
+                .AppendLine()
+                .Append('-', 10)
+                .Replace('-', '+')
+                .Remove(0, 10)
+                .Insert(0, new string('-', 10));
+
+            Console.WriteLine(builder);
+
+            Console.WriteLine("First character: " + builder[0]);
+        }
+    }
+}
