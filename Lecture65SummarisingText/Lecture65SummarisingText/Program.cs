bbd0251ef4ea4445d45a26a8054ebdb99774e0b8
Nichole Pellow <ladypellow@gmail.com>
2018-11-02 10:16:45 -0400

    Section 8
    Lecture 65: Summarising Text demo: practice files

---
+++ Lecture65SummarisingText/Lecture65SummarisingText/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture65SummarisingText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentence = "This is going to be a really really really really really super duper long text.";
+            var summary = StringUtility.SummerizeText(sentence, 25);
+            Console.WriteLine(summary);
+        }
+
+    }
+}
