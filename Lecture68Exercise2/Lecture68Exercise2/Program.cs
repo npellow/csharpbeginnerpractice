5d83fe955c0102ab3ad5706c49a7d76968185e63
Nichole Pellow <ladypellow@gmail.com>
2018-11-03 12:48:48 -0400

    Section 8: String Exercises
    Exercise 2

---
+++ Lecture68Exercise2/Lecture68Exercise2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture68Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a few numbers separated by hyphens (eg. 1-2-3-4): ");
+            var input = Console.ReadLine();
+
+            if (String.IsNullOrWhiteSpace(input))
+                return;
+
+            var numbers = new List<int>();
+            foreach (var number in input.Split('-'))
+                numbers.Add(Convert.ToInt32(number));
+
+            var uniques = new List<int>();
+            var includesDuplicates = false;
+            foreach (var number in numbers)
+            {
+                if (!uniques.Contains(number))
+                    uniques.Add(number);
+                else
+                {
+                    includesDuplicates = true;
+                    break;
+                }
+            }
+
+            if (includesDuplicates)
+                Console.WriteLine("Duplicate");
+        }
+    }
+}
