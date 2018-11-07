781c8c0835d6964b5dd752cef154942a1c92d636
Nichole Pellow <ladypellow@gmail.com>
2018-11-07 08:50:44 -0500

    Section 8: String Exercises
    Exercise 4

---
+++ Lecture68Exercise4/Lecture68Exercise4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture68Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a few words: ");
+            var input = Console.ReadLine();
+
+            if (String.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("Error");
+                return;
+            }
+
+            var variableName = "";
+            foreach (var word in input.Split(' '))
+            {
+                var wordWithPascalCase = char.ToUpper(word[0]) + word.ToLower().Substring(1);
+                variableName += wordWithPascalCase;
+            }
+
+            Console.WriteLine(variableName);
+        }
+    }
+}
