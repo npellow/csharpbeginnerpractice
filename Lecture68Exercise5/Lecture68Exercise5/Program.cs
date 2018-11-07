edbdb2836183b7e760d920136eddf0a6706687ed
Nichole Pellow <ladypellow@gmail.com>
2018-11-07 09:51:09 -0500

    Section 8: String Exercises
    Exercise 5

---
+++ Lecture68Exercise5/Lecture68Exercise5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture68Exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a word: ");
+            var input = Console.ReadLine();
+
+            var vowels = new List<char>(new char[] { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' });
+            var vowelsCount = 0;
+            foreach (var character in input)
+            {
+                if (vowels.Contains(character))
+                    vowelsCount++;
+            }
+
+            Console.WriteLine(vowelsCount);
+        }
+    }
+}
