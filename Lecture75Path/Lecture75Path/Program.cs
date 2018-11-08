b2b584aa2309e52ebba32c8fd29a6971b0c710ce
Nichole Pellow <ladypellow@gmail.com>
2018-11-08 10:50:17 -0500

    Section 9
    Lecture 75: Path demo practice

---
+++ Lecture75Path/Lecture75Path/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lecture75Path
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"c:\xammp\htdocs\index.php";
+
+            Console.WriteLine("Extension: " + Path.GetExtension(path));
+            Console.WriteLine("File Name: " + Path.GetFileName(path));
+            Console.WriteLine("File Name without Extension: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("Directory Name: " + Path.GetDirectoryName(path));
+        }
+    }
+}
