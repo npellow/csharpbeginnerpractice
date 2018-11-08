aede165681aacf4a376c4c08129acdea82b17ca5
Nichole Pellow <ladypellow@gmail.com>
2018-11-08 10:01:55 -0500

    Section 9
    Lecture 74: Directory and DirectoryInfo demo practice

---
+++ Lecture74DirectoryDirectoryInfo/Lecture74DirectoryDirectoryInfo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lecture74DirectoryDirectoryInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Directory examples
+            Directory.CreateDirectory(@"c:\temp\folder1");
+
+            var files = Directory.GetFiles(@"c:\xampp\htdocs", "*.php", SearchOption.AllDirectories);
+            foreach (var file in files)
+                Console.WriteLine(file);
+
+            var directories = Directory.GetDirectories(@"c:\xampp\htdocs", "*.*", SearchOption.AllDirectories);
+            foreach (var directory in directories)
+                Console.WriteLine(directory);
+
+            Directory.Exists("...");
+
+            //DirectoryInfo examples
+            var directoryInfo = new DirectoryInfo("...");
+            directoryInfo.GetFiles();
+            directoryInfo.GetDirectories();
+        }
+    }
+}
