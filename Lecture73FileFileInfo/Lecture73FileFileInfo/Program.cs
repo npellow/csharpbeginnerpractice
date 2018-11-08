01ed18ba26782003f07d3cc60e1d2cfee5309218
Nichole Pellow <ladypellow@gmail.com>
2018-11-08 09:20:50 -0500

    Section 9
    Lecture 73: Demo: File and FileInfo practice

---
+++ Lecture73FileFileInfo/Lecture73FileFileInfo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lecture73FileFileInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //File examples
+            var path = @"c:\somefile.jpg";
+
+            File.Copy(@"c:\temp\myfile.jpg", @"d:\temp\myfile.jpg", true);
+            File.Delete(path);
+            if (File.Exists(path))
+            {
+                //do something
+            }
+            var content = File.ReadAllText(path);
+
+            //FileInfo examples
+            var fileInfo = new FileInfo(path);
+
+            fileInfo.CopyTo("...");
+            fileInfo.Delete();
+            if (fileInfo.Exists)
+            {
+                //do something
+            }
+        }
+    }
+}
