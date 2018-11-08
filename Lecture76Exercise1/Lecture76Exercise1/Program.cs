324eda85523af739f600ba402fc64c8175a00ccd
Nichole Pellow <ladypellow@gmail.com>
2018-11-08 11:06:24 -0500

    Section 9 Exercises
    Exercise 1

---
+++ Lecture76Exercise1/Lecture76Exercise1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lecture76Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"c:\sampleFolder\test.txt";
+            var content = File.ReadAllText(path);
+
+            var words = content.Split(new char[] { ' ', '.' }, StringSplitOptions.RemoveEmptyEntries);
+
+            Console.WriteLine("Number of words in file: " + words.Length);
+        }
+    }
+}
